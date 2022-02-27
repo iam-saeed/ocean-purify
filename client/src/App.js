8be173e38ebc922f262f4c33cdadd5f336369063
Saeed Khan <saeed@skdevelopment.org>
2022-02-27 07:40:28 -0700

    react router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+import Landing from './pages/Landing/Landing.jsx';
+import Login from './pages/Login/Login.jsx';
+import Signup from './pages/Signup/Signup.jsx';
+import Dashboard from './pages/Dashboard/Dashboard.jsx';
+import Profile from './pages/Profile/Profile.jsx';
 
-
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Landing />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/signup" element={<Signup />} />
+      <Route exact path="/dashboard" element={<Dashboard />} />
+      <Route exact path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
 
