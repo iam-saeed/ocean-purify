2d547dc5ece25f9122466b1b2ba1fcf43d0a6cfc
Saeed Khan <saeed@skdevelopment.org>
2022-02-27 08:07:28 -0700

    added material ui dependencies - also completed signup and login form - keeping track of state

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Login = () => {
+  const [formValues, setFormValues] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+    console.log(formValues)
+  }
+  
+  const handleSubmit = () => {
+    console.log("submit")
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit}>
+      <Grid container alignItems="center" justify="center" direction="column">
+        <Grid item>
+          <AccountCircleIcon style={{ fontSize: "100px" }} />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="username-input"
+            name="username"
+            label="Username"
+            type="text"
+            value={formValues.username}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="password-input"
+            name="password"
+            label="Password"
+            type="password"
+            value={formValues.password}
+            onChange={handleChange}
+          />
+        </Grid>
+        <br />
+        <Grid item>
+          <a href="/signup">Create new account</a>
+        </Grid>
+        <br />
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </Grid>
+    </form>
   )
 }
 
--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Signup = () => {
+  const [formValues, setFormValues] = useState({
+    fullname: '',
+    email:'',
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+    console.log(formValues)
+  }
+  
+  const handleSubmit = () => {
+    console.log("submit")
+  }
   return (
-    <div>Signup</div>
+    <form onSubmit={handleSubmit}>
+    <Grid container alignItems="center" justify="center" direction="column">
+      <Grid item>
+        <AccountCircleIcon style={{ fontSize: "100px" }} />
+      </Grid>
+      <Grid item>
+        <TextField
+          id="fullname-input"
+          name="fullname"
+          label="Full Name"
+          type="text"
+          value={formValues.fullname}
+          onChange={handleChange}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          id="email-input"
+          name="email"
+          label="Email"
+          type="email"
+          value={formValues.email}
+          onChange={handleChange}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          id="username-input"
+          name="username"
+          label="Username"
+          type="text"
+          value={formValues.username}
+          onChange={handleChange}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          id="password-input"
+          name="password"
+          label="Password"
+          type="password"
+          value={formValues.password}
+          onChange={handleChange}
+        />
+      </Grid>
+      <br />
+      <Grid item>
+        <a href="/login">Already have an account?</a>
+      </Grid>
+      <br />
+      <Button variant="contained" color="primary" type="submit">
+        Submit
+      </Button>
+    </Grid>
+  </form>
   )
 }
 
