d4a4921ddb0bc65718621e8488d8c0c2654d2c17
Saeed Khan <saeed@skdevelopment.org>
2022-02-27 07:19:02 -0700

    barebones added for both client and server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const server = require("./api/server.js");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
