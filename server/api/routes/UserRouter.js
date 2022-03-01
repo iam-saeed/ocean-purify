7c9af244028b23a2f682c74cf3891a9424764e01
Saeed Khan <saeed@skdevelopment.org>
2022-03-01 09:37:47 -0700

    built signup and login endpoints - tested on postman, working as expected

--- server/api/routes/UserRouter.js
+++ server/api/routes/UserRouter.js
@@ -1,2 +1,37 @@
 const express = require('express');
+const User = require("../models/user-model.js");
+const bcrypt = require("bcryptjs");
 
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+    try {
+        const { fullname, email, username, password } = req.body;
+        const hash = await bcrypt.hashSync(password, 10);
+        const newUser = await User.addUser({
+            fullname: fullname,
+            email: email,
+            username: username,
+            password: hash
+        })
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.post("/login", (req, res) => {
+    try {
+        const password = req.body.password;
+        const verified = bcrypt.compareSync(password, req.formValues.password)
+        if(verified) {
+            res.json('Logged in')
+        } else {
+            res.status(401).json({ message: "Username or password is incorrect." })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+module.exports = router;
--- server/api/server.js
+++ server/api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const userRouter = require("../api/routes/UserRouter.js");
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+server.use("/users", userRouter);
 
 module.exports = server;
